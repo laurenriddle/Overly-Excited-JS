31e4e2e1bbfaa80d5e94c8eb591cf3787ec5b098
Lauren Riddle <lari7132@gmail.com>
2019-10-15 12:59:31 -0500

    changed the function in js to be an arrow function

--- overly-excited.js
+++ overly-excited.js
@@ -6,7 +6,7 @@ let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement(theWordArray, character, int) {
+ addExcitement = (theWordArray, character, int) => {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
